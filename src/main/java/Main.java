74335c3471462eed438c6fe7f2e3d3c49f2d9dac
Michael Schleichardt <michael@schleichardt.net>
2015-09-17 21:25:48 +0200

    x

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.err.println("hallo");
-        System.err.println(System.getenv("SOMEVAR"));
+        System.err.println(System.getenv());
     }
 }
